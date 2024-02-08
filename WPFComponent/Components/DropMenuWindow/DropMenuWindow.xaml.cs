5a4472955ddb5a4e997b1c6354bb842cac85be59
亚 陈 <2227237581@qq.com>
2024-02-09 00:21:42 +0800

    新增了图标等资源文件，增加了三级下拉菜单组件

---
+++ WPFComponent/Components/DropMenuWindow/DropMenuWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFComponent.Components.DropMenu
+{
+    /// <summary>
+    /// DropMenuWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class DropMenuWindow : Window
+    {
+        public DropMenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Index MenuItem is Clicked");
+        }
+    }
+}
